b5779c141794f0aaed02516b14b11b387c226b6d
benNjogu <kamandemachafi@gmail.com>
2023-01-15 17:08:00 +0300

    Section 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Download, Features, SectionWrapper } from "./components";
+import assets from "./assets";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">React Native Show case</h1>
-    </div>
+    <>
+      <SectionWrapper
+        title="Your own Store of Nifty NFTs. Start Selling & Growing."
+        description="Buy, Store, Collect NFTs, exchange and earn crypto. Join 25+ million people using ProNef Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Smart User Interface Marketplace."
+        description="Experience a Buttery UI of ProfNef NFT Marketplace. Smooth constant colors of a fluent UI design"
+        mockupImg={assets.homeCards}
+        reverse
+      />
+    </>
   );
 };
 
